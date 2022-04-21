dde8b427733d383fa16d8953de7956ba1770e2b3
MatthewKLewis <matthew.knight.lewis@gmail.com>
2022-04-21 13:15:31 -0400

    map readyish

--- src/app/components/shelf-map/shelf-map.component.ts
+++ src/app/components/shelf-map/shelf-map.component.ts
@@ -1,15 +1,177 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MapService } from '../../services/map.service';
+import { Feature, Map as OpMap, View } from 'ol/';
+import { Zoomify } from 'ol/source';
+import { getCenter } from 'ol/extent';
+import { ZoomSlider } from 'ol/control';
+import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import Projection from 'ol/proj/Projection';
 
 @Component({
   selector: 'app-shelf-map',
-  templateUrl: './shelf-map.component.html',
+  template: `<div class="map" id="map">{{errorMessage}}</div>`,
   styleUrls: ['./shelf-map.component.scss']
 })
-export class ShelfMapComponent implements OnInit {
+export class SharedMapComponent implements OnInit {
 
-  constructor() { }
+  @Input() data: any;
+  @Input() customerId: number = 0;
+  @Input() inputType: string = 'error!';
+
+  errorMessage: string = '';
+  mapId: number = 0;
+  mapData: any;
+  zones: any
+  zoneFeatures: any[] = []
+  pinFeature: any;
+
+  constructor(public mapService: MapService) { }
 
   ngOnInit(): void {
+    //console.log(this.data)
+    this.mapId = this.extractMapId(this.data);
+    this.pinFeature = this.extractPinFeature(this.data);
+    this.drawMap(1);
   }
+  drawMap(res: any) { //side effects
+    this.mapData = res;
+    this.processZones(this.mapData.Zones);
+    var extent: any = [0, 0, this.mapData.Width, -this.mapData.Height];
+    var projection = new Projection({
+      code: 'EPSG:4326',
+      units: 'pixels',
+      extent: [0, 0, this.mapData.Width, -this.mapData.Height],
+    });
+    var tileLayer = new TileLayer({
+      source: new Zoomify({
+        url: `../maps/${this.mapData.ModelId}/${this.mapData.ModelId}/`,
+        size: [this.mapData.Width, this.mapData.Height],
+      }),
+    });
+    var zoneLayer = new VectorLayer({
+      source: new VectorSource({ features: this.zoneFeatures }),
+    });
+    var vectorPinLayer = new VectorLayer({
+      source: new VectorSource({ features: [this.pinFeature] }),
+    });
+    var oldmap = document.querySelector('#map');
+    while (oldmap?.firstChild) {
+      oldmap.removeChild(oldmap.firstChild);
+    }
+    var mapView = new View({
+      projection: projection,
+      center: getCenter(extent),
+      zoom: 0,
+      maxZoom: 5,
+    });
+    const mapElement = <HTMLElement>document.querySelector('#map');
+    if (mapElement) {
+      var map = new OpMap({
+        layers: [tileLayer, zoneLayer, vectorPinLayer],
+        target: mapElement,
+        view: mapView,
+      });
+      var zoomslider = new ZoomSlider();
+      map.addControl(zoomslider);
+      map.on('pointermove', (event: any) => {
+        var layer = <any>map.getLayers().getArray()[1]
+        var feats = layer.getSource().getFeatures();
+        for (let i = 0; i < feats.length; i++) {
+          feats[i].setStyle(this.mapService.returnCustomZoneStyle(false))
+        }
+        map.forEachFeatureAtPixel(event.pixel, (feature: any) => {
+          if (feature &&feature?.values_?.FeatureType == 'zone') {
+            feature.setStyle(this.mapService.returnCustomZoneStyle(false, feature?.values_?.Name))
+          }
+        });
+      });
+      map.once('postrender', () => {
+        var breakdown = <any>this.pinFeature;
+        mapView.fit(breakdown.values_.geometry, {
+          padding: [400, 400, 400, 400],
+        });
+        console.log("Zoomed in on asset")
+      });
+    }
+    else {
+      console.log('map failed to draw.')
+    }
 
+  }
+  extractMapId(anyData: any): number {
+    let returnNumber = 0
+    switch (this.inputType) {
+      case "asset":
+        returnNumber = anyData.CurrentMapId
+        break;
+      case "device":
+        returnNumber = anyData.MapId
+        break;
+      case "infra":
+        returnNumber = anyData.MapId //re-mapped from ModelId
+        break;
+      default:
+        returnNumber = 0;
+        break;
+    }
+    return returnNumber;
+  }
+  extractPinFeature(anyData: any): Feature | null {
+    let returnFeature;
+    let altObject = {}
+    switch (this.inputType) {
+      case "asset":
+        returnFeature = this.mapService.createIconStaffFeature(anyData);
+        break;
+      case "device":
+        altObject = {
+          CurrentX: anyData.XCoordinate,
+          CurrentY: anyData.YCoordinate,
+          Name: anyData.UniqueId || "Device",
+          Id: anyData.Id
+        }
+        returnFeature = this.mapService.createIconStaffFeature(altObject);
+        break;
+      case "infra":
+        altObject = {
+          CurrentX: anyData.X,
+          CurrentY: anyData.Y,
+          Name: anyData.UniqueId || "Infra",
+          Id: anyData.Id
+        }
+        returnFeature = this.mapService.createIconStaffFeature(altObject);
+        break;
+      default:
+        returnFeature = null;
+        break;
+    }
+    //console.log(returnFeature)
+    return returnFeature;
+  }
+  processZones(zones: any) { //side effects
+    for (var i = 0; i < zones.length; i++) {
+      if (
+        zones[i].Polypoints[zones[i].Polypoints.length - 1] == ';'
+      ) {
+        zones[i].Polypoints = zones[i].Polypoints.slice(0, -1);
+      }
+      zones[i].Polypoints = zones[i].Polypoints.split(';');
+      for (var j = 0; j < zones[i].Polypoints.length; j++) {
+        zones[i].Polypoints[j] =
+          zones[i].Polypoints[j].split(',');
+        zones[i].Polypoints[j][0] = <Number>(
+          zones[i].Polypoints[j][0]
+        );
+        zones[i].Polypoints[j][1] = -(<Number>(
+          zones[i].Polypoints[j][1]
+        )); //YNEG
+      }
+    }
+    this.zones = zones;
+    for (let i = 0; i < this.zones.length; i++) {
+      this.zoneFeatures.push(this.mapService.createZoneFeature(this.zones[i], this.mapService.returnCustomZoneStyle()));
+    }
+  }
 }
---
+++ src/app/services/map.service.ts
@@ -0,0 +1,198 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import Icon from 'ol/style/Icon';
+import Feature from 'ol/Feature';
+import Fill from 'ol/style/Fill';
+import Point from 'ol/geom/Point';
+import Style from 'ol/style/Style';
+import Stroke from 'ol/style/Stroke';
+import { Text } from 'ol/style';
+import Polygon from 'ol/geom/Polygon';
+import GeometryLayout from 'ol/geom/GeometryLayout';
+import IconAnchorUnits from 'ol/style/IconAnchorUnits';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MapService {
+
+  customerId: number | undefined = 0;
+
+  constructor( private http: HttpClient) { }
+
+  initializeForInsert(newCustId: number) {
+    this.customerId = newCustId;
+  }
+
+  // API
+  getSiteHierarchy(): Observable<any> {
+    return this.http.get<any>(
+      `/api/MetaData/GetLocationHierarchy?CustomerId=${this.customerId}`,
+      {}
+    );
+  }
+  getMapById(id: number): Observable<any> {
+    return this.http.get<any>(
+      `/api/Maps/GetByMapId?MapId=${id}&CustomerId=${this.customerId}`,
+      {}
+    );
+  }
+
+  // RENDERING
+  infraIconStyle = new Style({
+    image: new Icon({
+      anchor: [0.5, 0.5],
+      anchorXUnits: IconAnchorUnits.FRACTION,
+      anchorYUnits: IconAnchorUnits.FRACTION,
+      src: './assets/images/ICON.png',
+    }),
+  });
+  infraIconStyle_New = new Style({
+    image: new Icon({
+      anchor: [0.5, 0.5],
+      anchorXUnits: IconAnchorUnits.FRACTION,
+      anchorYUnits: IconAnchorUnits.FRACTION,
+      src: './assets/images/ICON_NEW.png',
+    }),
+  });
+  invisibleIconStyle = new Style({
+    image: new Icon({
+      anchor: [0.5, 0.5],
+      anchorXUnits: IconAnchorUnits.FRACTION,
+      anchorYUnits: IconAnchorUnits.FRACTION,
+      src: './assets/images/invis.png',
+    }),
+  });
+  drawStyle = new Style({
+    stroke: new Stroke({
+      color: 'rgba(0, 0, 0, 0)',
+      width: 1,
+    }),
+  });
+  returnCustomZoneStyle(alertZone: boolean = false, text: string = "") {
+    var strokeColor = 'rgba(0, 157, 255)'; //default blue
+    var fillColor = 'rgba(0, 157, 255, 0.25)'; //default blue
+
+    if (alertZone) {
+      var strokeColor = 'Red'; //event red
+      var fillColor = 'rgba(232, 19, 19, 0.364)'; //event red
+    }
+
+    if (text) {
+      var tempStyle = new Style({
+        text: new Text({
+          textAlign: 'center',
+          textBaseline: 'hanging',
+          font: '14px Arial',
+          text: text,
+          fill: new Fill({ color: 'black' }),
+          stroke: new Stroke({ color: 'white', width: 2 }),
+          offsetX: 0,
+          offsetY: 0,
+          placement: 'POINT',
+          overflow: true,
+        }),
+        stroke: new Stroke({
+          color: strokeColor,
+          width: 1,
+        }),
+        fill: new Fill({
+          color: fillColor,
+        }),
+        zIndex: 5,
+      });
+      return tempStyle;
+    } else {
+      var tempStyle = new Style({
+        stroke: new Stroke({
+          color: strokeColor,
+          width: 1,
+        }),
+        fill: new Fill({
+          color: fillColor,
+        }),
+        zIndex: 5,
+      });
+      return tempStyle;
+    }
+  }
+  createAlertEventFeature(icon: any) {
+    var textStyle = new Style({
+      image: new Icon({
+        anchor: [0.5, 0.5],
+        anchorXUnits: IconAnchorUnits.FRACTION,
+        anchorYUnits: IconAnchorUnits.FRACTION,
+        src: './assets/images/alert.gif',
+      }),
+      text: new Text({
+        textAlign: 'center',
+        textBaseline: 'middle',
+        font: '12px Arial',
+        text: icon.Name,
+        fill: new Fill({ color: 'black' }),
+        stroke: new Stroke({ color: 'white', width: 1 }),
+        offsetX: 0,
+        offsetY: 0,
+        placement: 'POINT',
+        overflow: false,
+      }),
+    });
+    var tempFeature = new Feature({
+      geometry: new Point([icon.X, icon.Y, icon.X, icon.Y]),
+      Name: icon.Name,
+      UniqueId: icon.UniqueId,
+      Status: icon.Status,
+    });
+    tempFeature.setStyle(textStyle);
+    return tempFeature;
+  }
+  createIconStaffFeature(staff: any) {
+    var textStyle = new Style({
+      image: new Icon({
+        anchor: [0.5, 0.5],
+        anchorXUnits: IconAnchorUnits.FRACTION,
+        anchorYUnits: IconAnchorUnits.FRACTION,
+        src: staff.Icon || './assets/icons/star.png',
+      }),
+      text: new Text({
+        textAlign: 'center',
+        textBaseline: 'middle',
+        font: '12px Arial',
+        text: staff.Name || "Pin",
+        fill: new Fill({ color: 'black' }),
+        stroke: new Stroke({ color: 'white', width: 1 }),
+        offsetX: 0,
+        offsetY: 0,
+        placement: 'POINT',
+        overflow: false,
+      }),
+    });
+    var tempFeature = new Feature({
+      geometry: new Point([
+        staff.CurrentX,
+        -staff.CurrentY,
+        staff.CurrentX,
+        -staff.CurrentY,
+      ]),
+      Name: staff.Name,
+      UniqueId: staff.UniqueId,
+      Status: staff.Status,
+      Id: staff.Id,
+    });
+    tempFeature.setStyle(textStyle);
+    return tempFeature;
+  }
+  createZoneFeature(zone: any, style: any) {
+    var tempZoneFeature = new Feature({
+      FeatureType: 'zone',
+      geometry: new Polygon(Array(zone.Polypoints), GeometryLayout.XY),
+      Id: zone.Id,
+      Name: zone.Name,
+      point: Array(zone.Polypoints),
+      type: zone.ZoneType,
+    });
+    tempZoneFeature.setStyle(style);
+    return tempZoneFeature;
+  }
+}
