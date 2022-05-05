c8cb323b6d81bbd3d5ca88fb7c3855b2ae9a94ed
MatthewKLewis <matthew.knight.lewis@gmail.com>
2022-05-05 16:20:55 -0400

    player in angular now

--- src/app/components/video-stream/video-stream.component.ts
+++ src/app/components/video-stream/video-stream.component.ts
@@ -1,18 +1,28 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
+//import { loadPlayer, Player } from 'rtsp-relay/browser';
 
 @Component({
   selector: 'app-video-stream',
   templateUrl: './video-stream.component.html',
-  styleUrls: ['./video-stream.component.scss']
+  styleUrls: ['./video-stream.component.scss'],
 })
-export class VideoStreamComponent implements OnInit {
+export class VideoStreamComponent implements AfterViewInit {
+  /** the instance of the rtsp-relay client */
+  //player?: Player;
 
-  @ViewChild('streaming', {static: false}) streamingcanvas?: ElementRef; 
+  @ViewChild('videoPlayer')
+  videoPlayer?: ElementRef<HTMLCanvasElement>;
 
-  constructor() {
+  async ngAfterViewInit() {
+    // this will wait until the connection is established
+    // this.player = await loadPlayer({
+    //   url: 'ws://localhost:2000/api/stream',
+    //   canvas: this.videoPlayer!.nativeElement,
+
+    //   // optional
+    //   onDisconnect: () => console.log('Connection lost!'),
+    // });
+
+    //console.log('Connected!', this.player);
   }
-
-  ngOnInit(): void {
-  }
-
 }
