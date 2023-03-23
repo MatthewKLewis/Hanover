d3b69b47a0f690243d9989a8fb4575bec3cd29a7
MatthewKLewis <matthew.knight.lewis@gmail.com>
2023-03-23 10:52:00 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Hanover
 
 Dashboard incorporating an RTSP live stream from a Meraki Security Camera, an OpenLayers map, and business analytics.
+
+# Prep
+- Sign in to the Meraki Dashboard.
+- Forward RTSP to an address
+- Forward detection metadata to a local MQTT stream
+- Make sure the configs.conf file has the right IPs, ports, and serial numbers.
+
+# Startup
+- Start server.js in BackEnd.
+- Start main.py in object_detection_viewer_2
--- src/app/components/video-stream/video-stream.component.ts
+++ src/app/components/video-stream/video-stream.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
-import { loadPlayer, Player } from 'rtsp-relay/browser';
+//import { loadPlayer, Player } from 'rtsp-relay/browser';
 
 @Component({
   selector: 'app-video-stream',
@@ -8,11 +8,11 @@ import { loadPlayer, Player } from 'rtsp-relay/browser';
 })
 export class VideoStreamComponent implements AfterViewInit {
   //player?: Player;
-
   //@ViewChild('videoPlayer') videoPlayer?: ElementRef<HTMLCanvasElement>;
 
   async ngAfterViewInit() {
     console.log(location.host);
+    
     // this.player = await loadPlayer({
     //   //url: 'ws://' + location.host + '/api/stream',
     //   url: 'ws://' + 'localhost:2000' + '/api/stream',
